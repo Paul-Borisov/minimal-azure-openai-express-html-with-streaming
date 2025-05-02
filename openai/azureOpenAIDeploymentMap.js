8714de9b2af02b25690cd869e938afef4d6fccef
v-pborisov <v-pborisov@microsoft.com>
2025-05-02 09:02:39 +0300

    Map change, 1

--- openai/azureOpenAIDeploymentMap.js
+++ openai/azureOpenAIDeploymentMap.js
@@ -1,6 +1,6 @@
 // Here you can specify bespoke deployments in case if they do not match with model names unlike regular OpenAI.
 export const modelDeploymentMap = {
-  //"dall-e-3": "Dalle3",
+  "dall-e-3": "Dalle3",
   //"dall-e-2": "Dalle3",
   //"gpt-4o-mini-tts": "tts-hd"
 };
